2076cabc52024b4137a5282b39efad22a55b5f49
vagner santos <vagner131417@gmail.com>
2023-02-03 02:32:23 -0300

    index perfil

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,55 @@
-import Home from 'components/home-page/home'
-import { GetStaticProps, NextPage } from 'next'
-import { BlogPostProps } from 'interfaces/interface'
-import PageLayout from 'components/layouts/pageLayout'
-import { getDevtoPosts } from 'lib/fetchPosts'
+import Home from 'components/home-page/home';
+import PageLayout from 'components/layouts/pageLayout';
+import { Box, Flex, Text, Heading, useColorModeValue } from '@chakra-ui/react';
+import { useLinkColor } from 'components/theme'
 
-const Index: NextPage<BlogPostProps> = (props) => {
-  const { posts } = props
+const Index: React.FC = () => {
+  const linkColor = useLinkColor()
+  const bgColor = useColorModeValue('gray.50', '#202024');
+
   return (
-    <PageLayout title="Muhammad Ahmad - Full Stack Developer">
-      <Home posts={posts} />
+    <PageLayout title="Vagner Santos - Front End Developer">
+      <Home posts={[]} />
+      <Flex flexWrap="wrap" justifyContent="center" mb="">
+        <Box p={5} bg={bgColor} rounded="md" borderWidth="1px" maxW={['100%', '45%']} m={2}>
+          <Heading as="h3" size="4" pb="2" color={linkColor}>
+            Front-end
+          </Heading>
+          <Text fontSize="sm">
+            Atualmente estou trabalhando em projetos utilizando as skills React, NextJs, Styled
+            Components JavaScript, TypeScript...
+          </Text>
+        </Box>
+        <Box p={5} bg={bgColor} rounded="md" borderWidth="1px" maxW={['100%', '45%']} m={2}>
+          <Heading as="h3" size="4" pb="2" color={linkColor}>
+            UI/UX Design
+          </Heading>
+          <Text fontSize="sm">
+            Sou um entusiasta de código e design criativo para assim criar uma melhor experiência do
+            usuário na web.
+          </Text>
+        </Box>
+        <Box p={5} bg={bgColor} rounded="md" borderWidth="1px" maxW={['100%', '45%']} m={2}>
+          <Heading as="h3" size="4" pb="2" color={linkColor}>
+            WordPress
+          </Heading>
+          <Text fontSize="sm">
+            Experiência em desenvolvimento de sites e Landing pages com foco em performance e
+            usabilidade..
+          </Text>
+        </Box>
+        <Box p={5} bg={bgColor} rounded="md" borderWidth="1px" maxW={['100%', '45%']} m={2}>
+          <Heading as="h3" size="4" pb="2" color={linkColor}>
+            Freelance
+          </Heading>
+          <Text fontSize="sm">
+            "Iniciei minha jornada na área realizando projetos freelancer para amigos e empresas, o
+            que me proporcionou valiosa experiência."
+          </Text>
+        </Box>
+      </Flex>
     </PageLayout>
-  )
-}
+  );
+};
 
-export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getDevtoPosts()
-
-  if (!posts) {
-    return {
-      notFound: true,
-    }
-  }
-
-  return {
-    props: { posts },
-    revalidate: 1,
-  }
-}
-
-export default Index
+export default Index;
