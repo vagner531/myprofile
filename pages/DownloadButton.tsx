f12eac60460cb7f0ef4506da7349d0669c21256a
vagner santos <vagner131417@gmail.com>
2023-02-03 04:14:04 -0300

    mt update

--- pages/DownloadButton.tsx
+++ pages/DownloadButton.tsx
@@ -7,8 +7,10 @@ export function DownloadButton(): JSX.Element  {
     <Button 
       as="a"
       href="https://drive.google.com/file/d/1m5toXW5vrZ2ykFLfQ-lSIeX7l8kCwbcV/view?usp=sharing"
-      download target="_blank"
-      bg={bgColor} mt="5"
+      download 
+      target="_blank"
+      bg={bgColor} 
+      mt="5"
       borderWidth="1px"
       color="white"
     >
