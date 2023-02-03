8d5244c73c41d25269b78d0979d0c11343750f4e
vagner santos <vagner131417@gmail.com>
2023-02-03 04:58:32 -0300

    bg indextheme

--- components/theme/index.ts
+++ components/theme/index.ts
@@ -25,7 +25,7 @@ export const extendedTheme = extendTheme({
     global: (props) => ({
       body: {
         color: mode('gray.700', 'whiteAlpha.900')(props),
-        bg: mode('gray.50', 'gray.900')(props),
+        bg: mode('gray.50', 'rgb(18, 18, 20)')(props),
         fontSize: '1.2em',
         '.deleted': {
           color: '#ff8383 !important',
