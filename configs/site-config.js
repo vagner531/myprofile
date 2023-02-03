1e8fa7ddb1d2e2bec35bdbce4d32f1afccd26065
vagner santos <vagner131417@gmail.com>
2023-02-03 05:04:10 -0300

    siteconfig links

--- configs/site-config.js
+++ configs/site-config.js
@@ -1,55 +1,56 @@
 import {
-  FaGithub,
-  FaDev,
-  FaLinkedin,
-  FaQuora,
-  FaTwitter
+    FaGithub,
+    FaDev,
+    FaLinkedin,
+    FaQuora,
+    FaTwitter
 } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
 
 const siteConfig = {
-  copyright: `Copyright © ${new Date().getFullYear()} Muhammad Ahmad. All Rights Reserved.`,
-  author: {
-    name: "Muhammad Ahmad",
-    accounts: [
-      {
-        url: "https://github.com/MA-Ahmad",
-        label: "Github Account",
-        type: "gray",
-        icon: <FaGithub />
-      },
-      {
-        url: "https://twitter.com/muhammad_ahmaad",
-        label: "Twitter Account",
-        type: "twitter",
-        icon: <FaTwitter />
-      },
-      {
-        url: "https://dev.to/m_ahmad",
-        label: "Dev Account",
-        type: "gray",
-        icon: <FaDev />
-      },
-      {
-        url: "https://linkedin.com/in/muhammad-ahmad20",
-        label: "LinkedIn Account",
-        type: "linkedin",
-        icon: <FaLinkedin />
-      },
-      {
-        url: "https://www.quora.com/profile/Muhammad-Ahmad-66",
-        label: "Quora Account",
-        type: "red",
-        icon: <FaQuora />
-      },
-      {
-        url: "mailto:muhammad.ahmad8043@gmail.com",
-        label: "Mail ahmad",
-        type: "gray",
-        icon: <FiMail />
-      }
-    ]
-  }
+    copyright: `Copyright © ${new Date().getFullYear()} Vagner Santos. All Rights Reserved.`,
+    author: {
+        name: "Vagner Santos",
+        accounts: [{
+                /* // {
+                              url: "https://twitter.com/muhammad_ahmaad",
+                              label: "Twitter Account",
+                              type: "twitter",
+                              icon: <FaTwitter />
+                            */
+            },
+            {
+                url: "https://github.com/vagner531",
+                label: "Github Account",
+                type: "gray",
+                icon: < FaGithub / >
+            },
+            {
+                url: "https://dev.to/vagner531",
+                label: "Dev Account",
+                type: "gray",
+                icon: < FaDev / >
+            },
+            {
+                url: "https://www.linkedin.com/in/vagnersantosnascimento/",
+                label: "LinkedIn Account",
+                type: "linkedin",
+                icon: < FaLinkedin / >
+            },
+            // {
+            //   url: "https://www.quora.com/profile/Muhammad-Ahmad-66",
+            //   label: "Quora Account",
+            //   type: "red",
+            //   icon: <FaQuora />
+            // },
+            {
+                url: "mailto:vagnersantos.vagnerndsanto@gmail.com",
+                label: "Vagner Santos",
+                type: "gray",
+                icon: < FiMail / >
+            }
+        ]
+    }
 };
 
 export default siteConfig;
