61957583b468990dd40531435c15afcc002b1a0a
vagner santos <vagner131417@gmail.com>
2023-02-03 02:51:25 -0300

    data

--- data/data.ts
+++ data/data.ts
@@ -1,20 +1,37 @@
 export const companies = [
   {
-    title: 'Phaedra Solutions',
+    title: 'Agência Weber',
     alt: 'PS image',
-    role: 'Senior Software Engineer',
-    skills: ['ruby', 'rails', 'typescript', 'javascript', 'react', 'aws'],
-    period: '2019 - Present',
-    logo: '/assets/images/logos/phaedra.jpeg'
-  },
-  {
-    title: 'Brainx Technologies',
-    alt: 'BT image',
-    role: 'Software Engineer',
-    skills: ['ruby', 'rails', 'javascript', 'python'],
-    period: '2018 - 2019',
-    logo: '/assets/images/logos/brainxtech.png'
-  },
+    role: 'Desenvolvedor front end',
+    skills: [
+      'javascript',
+      'typescript',
+      'react',
+      'Wordpress',
+      'Elementor',
+      'HTML5',
+      'CSS3',
+      ' SASS',
+      'Flexbox',
+      'GRID',
+      'Bootstrap',
+      'jquery',
+      'Figma',
+      'Adobe Photoshop',
+      'SEO',
+      'UX'
+    ],
+    period: '2021-2022',
+    logo: '/assets/images/logos/agencia-weber.png'
+  }
+  // {
+  //   title: 'Brainx Technologies',
+  //   alt: 'BT image',
+  //   role: 'Software Engineer',
+  //   skills: ['ruby', 'rails', 'javascript', 'python'],
+  //   period: '2018 - 2019',
+  //   logo: '/assets/images/logos/brainxtech.png'
+  // },
   // {
   //   title: "Codenya Studio",
   //   alt: "CS image",
@@ -23,55 +40,111 @@ export const companies = [
   //   period: "2018 - Present",
   //   logo: '/assets/images/logos/codenya.png'
   // },
-  {
-    title: 'Brainplow',
-    alt: 'Brainplow image',
-    role: 'Software Engineer',
-    skills: ['javascript', 'python', 'angular'],
-    period: '2017 - 2018',
-    logo: '/assets/images/logos/brainplow.jpg'
-  }
+  // {
+  //   title: 'Brainplow',
+  //   alt: 'Brainplow image',
+  //   role: 'Software Engineer',
+  //   skills: ['javascript', 'python', 'angular'],
+  //   period: '2017 - 2018',
+  //   logo: '/assets/images/logos/brainplow.jpg'
+  // }
 ];
 
 export const institutes = [
   {
-    short_title: 'FAST - NUCES',
-    title: 'National University of Computer and Emerging Sciences',
+    short_title: 'FIAP',
+    title: ' Faculdade de Informática e Administração Paulista (FIAP)',
     alt: 'Nuces image',
-    role: "Bachelor's Degree in Computer Science",
-    skills: ['project management', 'web development', 'data structures'],
-    period: '2013 - 2017',
+    role: 'Análise e Desenvolvimento de SistemasAnálise e Desenvolvimento de Sistemas',
+    skills: ['System Analysis', 'Web 2.0 Developer and Designer', 'Mobile Developer'],
+    period: '2019 - 2020',
     startingYear: '2013',
-    logo: '/assets/images/logos/fast.png',
-    awards: [
-      {
-        title: 'Speed Programming Battle',
-        description: 'Won speed programming battle which held at FAST - NUCES.',
-        date: 'May 1, 2015'
-      },
-      {
-        title: 'Silver Medal',
-        description:
-          'This medal was awarded to me by FAST - NUCES for getting a CGPA of 3.9 in 2nd semester.',
-        date: 'May 1, 2014'
-      },
-      {
-        title: 'Speed Programming Battle',
-        description: 'Won speed programming battle which held at FAST - NUCES.',
-        date: 'Nov 1, 2013'
-      }
-    ]
+    logo: '/assets/images/logos/fiap.png'
+    // awards: [
+    //   {
+    //     title: 'Speed Programming Battle',
+    //     description: 'Won speed programming battle which held at FAST - NUCES.',
+    //     date: 'May 1, 2015'
+    //   },
+    //   {
+    //     title: 'Silver Medal',
+    //     description:
+    //       'This medal was awarded to me by FAST - NUCES for getting a CGPA of 3.9 in 2nd semester.',
+    //     date: 'May 1, 2014'
+    //   },
+    //   {
+    //     title: 'Speed Programming Battle',
+    //     description: 'Won speed programming battle which held at FAST - NUCES.',
+    //     date: 'Nov 1, 2013'
+    //   }
+    // ]
+  },
+  {
+    short_title: 'Rocketseat',
+    title: 'Bootcamp Ignite - Rocketseat',
+    alt: 'Nuces image',
+    role: 'Programação fullstack passando por todo o processo do desenvolvimento de sistemas',
+    skills: ['Trilha ReactJS', 'Trilha Node.JS', 'Trilha React Native'],
+    period: '2022-2023',
+    startingYear: '2013',
+    logo: '/assets/images/logos/rocket.jpg'
+    // awards: [
+    //   {
+    //     title: 'Speed Programming Battle',
+    //     description: 'Won speed programming battle which held at FAST - NUCES.',
+    //     date: 'May 1, 2015'
+    //   },
+    //   {
+    //     title: 'Silver Medal',
+    //     description:
+    //       'This medal was awarded to me by FAST - NUCES for getting a CGPA of 3.9 in 2nd semester.',
+    //     date: 'May 1, 2014'
+    //   },
+    //   {
+    //     title: 'Speed Programming Battle',
+    //     description: 'Won speed programming battle which held at FAST - NUCES.',
+    //     date: 'Nov 1, 2013'
+    //   }
+    // ]
+  },
+  {
+    short_title: 'Rocketseat',
+    title: 'React (Alura)',
+    alt: 'Nuces image',
+    role: 'Componentes reutilizáveis para sua webappReact',
+    skills: ['Validação', 'Rotas', 'Integração com API'],
+    period: '2020',
+    startingYear: '2013',
+    logo: '/assets/images/logos/alura.jpg'
+    // awards: [
+    //   {
+    //     title: 'Speed Programming Battle',
+    //     description: 'Won speed programming battle which held at FAST - NUCES.',
+    //     date: 'May 1, 2015'
+    //   },
+    //   {
+    //     title: 'Silver Medal',
+    //     description:
+    //       'This medal was awarded to me by FAST - NUCES for getting a CGPA of 3.9 in 2nd semester.',
+    //     date: 'May 1, 2014'
+    //   },
+    //   {
+    //     title: 'Speed Programming Battle',
+    //     description: 'Won speed programming battle which held at FAST - NUCES.',
+    //     date: 'Nov 1, 2013'
+    //   }
+    // ]
   }
 ];
 
 export const skillsArray = [
-  {
-    name: 'Ruby on Rails',
-    description: 'Web development',
-    link: 'https://rubyonrails.org/',
-    type: 'development',
-    image: '/assets/images/skills/rails.png'
-  },
+  // {
+  //   name: 'Ruby on Rails',
+  //   description: 'Web development',
+  //   link: 'https://rubyonrails.org/',
+  //   type: 'development',
+  //   image: '/assets/images/skills/rails.png'
+  // },
   {
     name: 'React',
     description: 'Web development',
@@ -80,19 +153,26 @@ export const skillsArray = [
     image: '/assets/images/skills/react.png'
   },
   {
-    name: 'Ruby',
-    description: 'Web development, scrapping',
-    link: 'https://www.ruby-lang.org/en/',
+    name: 'Next.js',
+    description: 'Web development',
+    link: 'https://nextjs.org/',
     type: 'development',
-    image: '/assets/images/skills/ruby.png'
+    image: '/assets/images/skills/nextjs.png'
   },
-  {
-    name: 'Python',
-    description: 'Web development, scrapping',
-    link: 'https://www.python.org/',
-    type: 'development',
-    image: '/assets/images/skills/python.png'
-  },
+  // {
+  //   name: 'Ruby',
+  //   description: 'Web development, scrapping',
+  //   link: 'https://www.ruby-lang.org/en/',
+  //   type: 'development',
+  //   image: '/assets/images/skills/ruby.png'
+  // },
+  // {
+  //   name: 'Python',
+  //   description: 'Web development, scrapping',
+  //   link: 'https://www.python.org/',
+  //   type: 'development',
+  //   image: '/assets/images/skills/python.png'
+  // },
   {
     name: 'Javascript',
     description: 'Web development',
@@ -108,18 +188,39 @@ export const skillsArray = [
     image: '/assets/images/skills/typescript.png'
   },
   {
-    name: 'Html5',
+    name: 'HTML5',
     description: 'Web layouts',
     link: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5',
     type: 'development',
     image: '/assets/images/skills/html-5.png'
   },
   {
-    name: 'Css3',
+    name: 'CSS3',
     description: 'Web design',
     link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
     type: 'design',
     image: '/assets/images/skills/css3.png'
+  },
+  {
+    name: 'Styled Components',
+    description: 'Web design',
+    link: 'https://styled-components.com/',
+    type: 'design',
+    image: 'assets/images/skills/styled.png'
+  },
+  {
+    name: 'Tailwindcss',
+    description: 'Web design',
+    link: 'https://tailwindcss.com/',
+    type: 'design',
+    image: 'assets/images/skills/tailwindcss.png'
+  },
+  {
+    name: 'Chakra UI',
+    description: 'Web design',
+    link: 'https://chakra-ui.com/',
+    type: 'design',
+    image: 'assets/images/skills/chakraui.png'
   },
   {
     name: 'Bootstrap',
@@ -143,12 +244,19 @@ export const skillsArray = [
     image: '/assets/images/skills/mysql.png'
   },
   {
-    name: 'Redis',
-    description: 'Database',
-    link: 'https://redis.io/',
-    type: 'database',
-    image: '/assets/images/skills/redis.png'
+    name: 'Node.js',
+    description: 'Backend',
+    link: 'https://nodejs.org/en/',
+    type: 'Backend',
+    image: '/assets/images/skills/node.png'
   },
+  // {
+  //   name: 'Redis',
+  //   description: 'Database',
+  //   link: 'https://redis.io/',
+  //   type: 'database',
+  //   image: '/assets/images/skills/redis.png'
+  // },
   {
     name: 'Heroku',
     description: 'Devops',
@@ -156,26 +264,13 @@ export const skillsArray = [
     type: 'devops',
     image: '/assets/images/skills/heroku.png'
   },
+
   {
-    name: 'AWS',
-    description: 'Devops',
-    link: 'https://aws.amazon.com/',
-    type: 'devops',
-    image: 'assets/images/skills/aws.png'
-  },
-  {
-    name: 'Digital Ocean',
-    description: 'Devops',
-    link: 'https://www.digitalocean.com/',
-    type: 'devops',
-    image: '/assets/images/skills/digital-ocean.png'
-  },
-  {
-    name: 'Nginx',
-    description: 'Web server',
-    link: 'https://www.nginx.com/',
-    type: 'devops',
-    image: '/assets/images/skills/nginx.png'
+    name: 'Git',
+    description: 'Version control',
+    link: 'https://github.com/vagner531',
+    type: 'git',
+    image: '/assets/images/skills/git.png'
   },
   {
     name: 'Visual Studio Code',
@@ -190,6 +285,13 @@ export const skillsArray = [
     link: 'https://www.figma.com/',
     type: 'design',
     image: '/assets/images/skills/figma.png'
+  },
+  {
+    name: 'Photoshop',
+    description: 'Web design',
+    link: 'https://www.adobe.com/br/products/photoshop.html',
+    type: 'design',
+    image: '/assets/images/skills/photoshop.png'
   }
 ];
 
