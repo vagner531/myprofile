00fc2acfc861a4d7d5a9054b1ec5327b3e09f79d
vagner santos <vagner131417@gmail.com>
2023-02-03 04:16:45 -0300

    react

--- pages/DownloadButton.tsx
+++ pages/DownloadButton.tsx
@@ -1,4 +1,5 @@
 import { Button, Flex, useColorModeValue } from '@chakra-ui/react';
+import React from 'react';
 
 export function DownloadButton(): JSX.Element  {
   const bgColor = useColorModeValue('gray.00', '#202024');
